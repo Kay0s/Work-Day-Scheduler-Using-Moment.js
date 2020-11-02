cfe79d5f8ca97bf30b347edf3ae927b8066e0bc2
Kay0s <hamilton.kristina@gmail.com>
2020-11-01 20:01:52 -0600

    updated css styling

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ $(document).ready(function () {
 
 //getting and parsing info from storage
 let storageSchedule = JSON.parse(localStorage.getItem("storageSchedule"));
+storageSchedule.forEach((pair) => {
+  console.log($(`.description`));
 
+  console.log($(`.description[data-time=9]`));
+  // this part is the selector. it selects a classname with the attribute data-time = to the number that is passed from each object in array.
+  // $(`.description[data-time='${pair.key}']`)
+
+  //.val(pair.value) that sets the value you are passing to the element you selected in the first section, the comments above.
+  $(`.description[data-time='${pair.key}']`).val(pair.value);
+});
 //checking to see if info is already in storage, if there isn't storageSchedule array is created
 if (storageSchedule === null) {
   storageSchedule = [];
@@ -21,11 +30,7 @@ let userInputSlot = document.querySelector(".time");
 $(".saveBtn").on("click", function (event) {
   event.preventDefault();
 
-  let inputComp = $(this)
-    .parent()
-    .siblings(".td-time")
-    .children()
-    .closest("input");
+  let inputComp = $(this).siblings(".description");
   let key = inputComp.attr("data-time");
   let value = inputComp.val();
 
@@ -35,24 +40,21 @@ $(".saveBtn").on("click", function (event) {
   localStorage.setItem("storageSchedule", JSON.stringify(storageSchedule));
 });
 
-// array for data
-console.log($(".time-input[data-time='10']"));
-
 //.each loop to add CSS .present, .past and .future classes based on current time as defined by moment().hours()
 //in comparison to "data-time" on the table
 let timeId = moment().hours();
 
-$(".table-row").each(function () {
-  let tableInt = $(this).children(".td-time").children().attr("data-time");
+$(".description").each(function () {
+  let tableInt = $(this).attr("data-time");
+  $(this).removeClass("past");
+  $(this).removeClass("present");
+  $(this).removeClass("future");
+
   if (parseInt(tableInt) === timeId) {
     $(this).addClass("present");
-  } else if (parseInt(tableInt) < timeId) {
-    $(this).removeClass("present");
-    $(this).addClass("past");
-  }
-  if (parseInt(tableInt) > timeId) {
-    $(this).removeClass("past");
-    $(this).removeClass("present");
+  } else if (parseInt(tableInt) > timeId) {
     $(this).addClass("future");
+  } else {
+    $(this).addClass("past");
   }
 });
